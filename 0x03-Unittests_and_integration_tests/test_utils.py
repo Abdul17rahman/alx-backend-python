204b5ca6649fef189b076a6fbeb4f78834e0d638
Abdul Rahman <abdulnsamba@gmail.com>
2024-06-02 11:35:27 +0300

    test access fix

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -10,7 +10,6 @@ from utils import access_nested_map
 
 class TestAccessNestedMap(unittest.TestCase):
 
-	def test_access_nested_map(self):
-		""" Testing the access_nested_map function"""
-		self.assertEqual(access_nested_map({"a": 1}, path=("a",)), 1)
-		self.assertEqual(access_nested_map({"a": {"b": 2}}, path=("a",)), {'b': 2})
+    def test_access_nested_map(self):
+        self.assertEqual(access_nested_map({"a": 1}, path=("a",)), 1)
+        self.assertEqual(access_nested_map({"a": {"b": 2}}, path=("a",)), {'b': 2})
