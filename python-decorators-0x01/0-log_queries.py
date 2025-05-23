8838748a3046093554ba19edcc966342f38c46b3
Abdul Rahman <abdulnsamba@gmail.com>
2025-05-23 12:08:37 +0300

    datetime

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,13 +1,16 @@
 import sqlite3
 import functools
+from datetime import datetime
 
-#### decorator to lof SQL queries
+# decorator to lof SQL queries
+
 
 def log_queries(fn):
     def wrapper(*args, **kwargs):
         print(kwargs['query'])
         fn(*args, **kwargs)
     return wrapper
+
 
 @log_queries
 def fetch_all_users(query):
@@ -18,5 +21,6 @@ def fetch_all_users(query):
     conn.close()
     return results
 
-#### fetch users while logging the query
+
+# fetch users while logging the query
 users = fetch_all_users(query="SELECT * FROM users")
