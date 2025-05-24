6951d0e54ef237cc4fbdadab9c1c907b71253131
Abdul Rahman <abdulnsamba@gmail.com>
2025-05-24 13:52:09 +0300

    async

---
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -0,0 +1,36 @@
+import asyncio
+import aiosqlite
+import os
+
+db_path = os.path.join(os.getcwd(), "../python-decorators-0x01/users.db")
+
+
+async def async_fetch_users():
+    async with aiosqlite.connect(db_path) as db:
+        async with db.execute("SELECT * FROM users") as cursor:
+            users = await cursor.fetchall()
+            print("All users:")
+            for user in users:
+                print(user)
+            return users
+
+
+async def async_fetch_older_users():
+    async with aiosqlite.connect(db_path) as db:
+        async with db.execute("SELECT * FROM users WHERE id = 1") as cursor:
+            older_users = await cursor.fetchall()
+            print("\nFirst User:")
+            for user in older_users:
+                print(user)
+            return older_users
+
+
+async def fetch_concurrently():
+    # Run both queries concurrently
+    await asyncio.gather(
+        async_fetch_users(),
+        async_fetch_older_users()
+    )
+
+
+asyncio.run(fetch_concurrently())
