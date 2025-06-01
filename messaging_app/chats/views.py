4b8c976273a66a382456b0d257fda955f1ead92b
Abdul Rahman <abdulnsamba@gmail.com>
2025-06-01 15:01:40 +0300

    added filters

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, filters, status
 from .models import Conversation, Message
 from .serializers import MessageSerializer, ConversationSerializer
 
@@ -7,8 +7,13 @@ from .serializers import MessageSerializer, ConversationSerializer
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all().order_by('sent_at')
     serializer_class = MessageSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['message_body', 'sent_at']
 
 
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
+    filter_backends = [filters.OrderingFilter, filters.SearchFilter]
+    ordering_fields = ['created_at']
+    search_fields = ['participants_first_email']
