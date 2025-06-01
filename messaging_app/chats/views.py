2db4337c7c5b08b9fc86a8c9bb603f1058dc6bfd
Abdul Rahman <abdulnsamba@gmail.com>
2025-06-01 14:20:56 +0300

    added viewsets

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Conversation, Message
+from .serializers import MessageSerializer, ConversationSerializer
 
-# Create your views here.
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all().order_by('sent_at')
+    serializer_class = MessageSerializer
+
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
