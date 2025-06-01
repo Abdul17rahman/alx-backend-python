14d3e6a04874c06088332e8e25137e3c0784dddc
Abdul Rahman <abdulnsamba@gmail.com>
2025-06-01 09:59:25 +0300

    serializers: added validation

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -4,10 +4,16 @@ from .models import User, Conversation, Message
 
 
 class UserSerializer(serializers.ModelSerializer):
+    full_name = serializers.SerializerMethodField()
+
     class Meta:
         model = User
-        fields = ["email", "first_name", "last_name", "phone_number"]
+        fields = ["email", "first_name",
+                  "last_name", "phone_number", "full_name"]
         read_only_fields = ['email']
+
+    def get_full_name(self, obj):
+        return f"{obj.first_name} {obj.last_name}"
 
 
 class MessageSerializer(serializers.ModelSerializer):
@@ -23,8 +29,16 @@ class ConversationSerializer(serializers.ModelSerializer):
     participants = UserSerializer(many=True, read_only=True)
     messages = MessageSerializer(many=True, read_only=True)
 
+    title = serializers.CharField(write_only=True, required=False)
+
     class Meta:
         model = Conversation
-        fields = ['conversation_id',
-                  'participants', 'created_at', 'messages']
+        fields = ['conversation_id', 'participants',
+                  'created_at', 'messages', 'title']
         read_only_fields = ['conversation_id']
+
+    def validate_title(self, value):
+        if "badword" in value.lower():
+            raise serializers.ValidationError(
+                "Title contains inappropriate content.")
+        return value
