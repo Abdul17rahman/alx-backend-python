b2adc94895524834bfd430cc0f459591e36d33f9
Abdul Rahman <abdulnsamba@gmail.com>
2025-06-05 16:20:00 +0300

    added is participant permission

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets, filters, status, permissions
 from .models import Conversation, Message
 from .serializers import MessageSerializer, ConversationSerializer
+from .permissions import IsParticipantOfConversation
 
 
 class MessageViewSet(viewsets.ModelViewSet):
@@ -20,4 +21,5 @@ class ConversationViewSet(viewsets.ModelViewSet):
     ordering_fields = ['created_at']
     search_fields = ['participants_first_email']
 
-    permission_class = [permissions.IsAuthenticated]
+    permission_class = [permissions.IsAuthenticated,
+                        IsParticipantOfConversation]
