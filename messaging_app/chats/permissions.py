815696cbc3806c4f471a9c77e2e60f8f15ac65d5
Abdul Rahman <abdulnsamba@gmail.com>
2025-06-08 13:23:27 +0300

    permissions

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -3,6 +3,11 @@ from rest_framework import permissions
 
 class IsParticipantOfConversation(permissions.BasePermission):
 
+    """
+    Allows only authenticated participants of the conversation to send (POST),
+    view (GET), update (PUT/PATCH), or delete (DELETE).
+    """
+
     def has_object_permission(self, request, view, obj):
         if not request.user or not request.user.is_authenticated:
             return False
