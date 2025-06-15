d6d16077a6a864b51580c1dca6d951d109b4af8d
Abdul Rahman <abdulnsamba@gmail.com>
2025-06-15 15:10:05 +0300

    signal for logging msg edit

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -10,6 +10,7 @@ class Message(models.Model):
         User, related_name='received_messages', on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
+    edited = models.BooleanField(default=False)
 
     def __str__(self):
         return f"From {self.sender} to {self.receiver} at {self.timestamp}"
@@ -23,3 +24,13 @@ class Notification(models.Model):
 
     def __str__(self):
         return f"Notification for {self.user} - Message ID: {self.message.id}"
+
+
+class MessageHistory(models.Model):
+    message = models.ForeignKey(
+        Message, on_delete=models.CASCADE, related_name='history')
+    old_content = models.TextField()
+    archived_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"History of Message ID {self.message.id} at {self.archived_at}"
